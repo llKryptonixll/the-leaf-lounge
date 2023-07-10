3077329e839fd256e4f9c5691eae6856bcf26b02
Lucas Cerri <lucascerri3@outlook.de>
2023-07-10 10:43:25 +0200

    category section added

--- src/pages/home/components/PlantCategories.jsx
+++ src/pages/home/components/PlantCategories.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PlantCategories = () => {
   return (
-    <div>PlantCategories</div>
+    <section className='category_section'>
+      <div className="header">
+        <p>All kind of Plants</p>
+        <p>Discover Category</p>
+      </div>
+
+      <div className='image_container'>
+        <div className='large_image_wrapper'>
+          <img src="images/home-images/category_plant1.jpg" alt="plant-category-image" />
+          <Link to={"/shop"} className='link'>Small Plants <sup>(7)</sup></Link>
+        </div>
+
+        <div className='small_image_container'>
+          <div className='small_image_wrapper'>
+            <img src="images/home-images/category_plant2.jpg" alt="plant-category-image" />
+            <Link to={"/shop"} className='link'>Succulents <sup>(3)</sup></Link>
+          </div>
+          <div className='small_image_wrapper'>
+            <img src="images/home-images/category_plant3.jpg" alt="plant-category-image" />
+            <Link to={"/shop"} className='link'>Potter Plants <sup>(4)</sup></Link>
+          </div>
+          <div className='small_image_wrapper'>
+            <img src="images/home-images/category_plant4.jpg" alt="plant-category-image" />
+            <Link to={"/shop"} className='link'>Terrariums <sup>(1)</sup></Link>
+          </div>
+          <div className='small_image_wrapper'>
+            <img src="images/home-images/category_plant5.jpg" alt="plant-category-image" />
+            <Link to={"/shop"} className='link'>Cactuses <sup>(3)</sup></Link>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
