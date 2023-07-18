8e1c895d988fd0270a0a046326b777ac63d6eef0
Lucas Cerri <lucascerri3@outlook.de>
2023-07-18 14:05:21 +0200

    mapbox section created ad mabox installed

--- src/pages/contact/components/Mapbox.jsx
+++ src/pages/contact/components/Mapbox.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map, {
+  Marker,
+  NavigationControl,
+  FullscreenControl,
+  GeolocateControl,
+} from "react-map-gl";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const Mapbox = () => {
-  return (
-    <div>Mapbox</div>
-  )
+
+    const API_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
+    const [lng, setLng] = useState(-105.31581418746562);
+    const [lat, setLat] = useState(39.643772795122445);
+
+    return (
+        <section className='mapbox_section'>
+            <Map
+                mapboxAccessToken={API_TOKEN}
+                style={{
+                width: "100%",
+                height: "100%",
+                }}
+                scrollZoom={false}
+                initialViewState={{
+                longitude: lng,
+                latitude: lat,
+                zoom: 13,
+                }}
+                mapStyle="mapbox://styles/kryptonix/clk88b3xg00l301pfh6gdehrp"
+            >
+                <Marker longitude={lng} latitude={lat}>
+                    <div className="map_marker">
+                        <FontAwesomeIcon className="icon" icon={faLocationDot}/>
+                    </div>
+                </Marker>
+                <NavigationControl position="bottom-right" />
+                <FullscreenControl />
+                <GeolocateControl />
+            </Map>
+        </section>
+    )
 }
 
 export default Mapbox
