37e2da0134a2a8de5e0d9727e2606adc723d56ad
Lucas Cerri <lucascerri3@outlook.de>
2023-08-21 15:22:44 +0200

    contact form validated with react hook form

--- src/pages/contact/components/ContactForm.jsx
+++ src/pages/contact/components/ContactForm.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const inputErrorStyles = {
+    backgroundColor: "#FF4D4D",
+    outline: "none"
+  }
+  const errorMessageStyles = {
+    fontWeight: 600
+  }
+  const onSubmit = data => console.log(data); 
+
   return (
     <section className="form_section">
       <div className="form_header">
@@ -8,18 +20,55 @@ const ContactForm = () => {
         <p>Empowering individuals to become leaf enthusiasts - a compilation of articles crafted by the Leaf Lounge team of Plant Enthusiasts.</p>
       </div>
       
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="input_wrapper">
           <label htmlFor="name">Your Name</label>
-          <input placeholder='e.g. Lucas Cerri' id='name' name='name' type="text" />
+          <input
+            style={errors.name && {...inputErrorStyles}}
+            {...register("name", {
+              required: "This field is required",
+              pattern: {
+                value: /^[^\d]+$/,
+                message: "Can not contain numbers"
+              }
+            })}
+            placeholder='e.g. Lucas Cerri'
+            type="text"
+            name='name'
+            id='name'
+          />
+          {errors.name && <span style={{...errorMessageStyles}}>{errors.name.message}</span>}
         </div>
         <div className="input_wrapper">
           <label htmlFor="email">Your Email Address</label>
-          <input placeholder='e.g. leafLounge@mail.com' id='email' name='email' type="text" />
+          <input
+            style={errors.email && {...inputErrorStyles}}
+            {...register("email", {
+              required: "This field is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Please enter a correct email address"
+              }
+            })}
+            placeholder='e.g. leafLounge@mail.com'  
+            type="text"
+            name='email'
+            id='email'
+          />
+          {errors.email && <span style={{...errorMessageStyles}}>{errors.email.message}</span>}
         </div>
         <div className='input_wrapper'>
           <label htmlFor='message'>Your Message</label>
-          <textarea placeholder='e.g. Hello I need an advice on which plants would suit my house' name="message" id="message"></textarea>
+          <textarea 
+            style={errors.message && {...inputErrorStyles}}
+            {...register("message", { 
+              required: true 
+            })} 
+            placeholder='e.g. Hello I need an advice on which plants would suit my house' 
+            name="message" 
+            id="message">
+          </textarea>
+          {errors.message && <span style={{...errorMessageStyles}}>This field is required</span>}
         </div>
         <button className='submit_button' type='submit'>Contact Us</button>
       </form>
