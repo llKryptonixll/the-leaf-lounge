5b78ed372518d3fdcdcdcec939afe1fd1acc0533
Lucas Cerri <lucascerri3@outlook.de>
2023-08-27 07:45:05 +0200

    added gulp to project

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+import gulp from 'gulp';
+import gulpWebp from 'gulp-webp';
+import imagemin from 'gulp-imagemin';
+
+async function compressImages() {
+    gulp.src(['public/images/**/*'])
+    .pipe(imagemin())
+    .pipe(gulpWebp())
+    .pipe(gulp.dest('public/compressedImages'))
+}
+
+export default gulp.series(compressImages);
