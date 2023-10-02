e947a9c6bf997ecf77791429c053e6928864fbd5
Lucas Cerri <lucascerri3@outlook.de>
2023-10-02 15:43:52 +0200

    started with creating animations
    installed framer motion and started to create animations in homepage

--- src/pages/home/components/DecorateSection.jsx
+++ src/pages/home/components/DecorateSection.jsx
@@ -1,50 +1,87 @@
+import { motion } from "framer-motion"
+
 const DecorateSection = () => {
+  const headerSpan1Animation = {
+    initial: { opacity: 0, left: -60 },
+    whileInView: { opacity: 1, left: 0 },
+    transition: { duration: 0.5 },
+    viewport: { once: true }
+  }
+  const headerSpan2Animation = {
+    initial: { opacity: 0, right: -60 },
+    whileInView: { opacity: 1, right: 0 },
+    transition: { duration: 0.5, delay: 0.1},
+    viewport: { once: true }
+  }
+  const mainTextAnimation = {
+    initial: { opacity: 0, top: -60 },
+    whileInView: { opacity: 1, top: 0 },
+    transition: { duration: 0.5, delay: 0.3 },
+    viewport: { once: true }
+  }
+  const generalIconAnimation = {
+    initial: { opacity: 0, top: -60 },
+    whileInView: { opacity: 1, top: 0 },
+    viewport: { once: true }
+  }
+  const generalIconContentAnimation = {
+    initial: { opacity: 0, right: -60 },
+    whileInView: { opacity: 1, right: 0 },
+    viewport: { once: true }
+  }
+  const plantImageAnimation = {
+    initial: { opacity: 0, scale: 0 },
+    whileInView: { opacity: 1, scale: 1 },
+    transition: { duration: 0.5, delay: 0.8 },
+    viewport: { once: true }
+  }
   return (
     <section className="decorate_section">
       <article className='article_container'>
         <div className="text_wrapper">
           <h2 className='header'>
-            Decorate your home <span>with plants</span>
+            <motion.span {...headerSpan1Animation}>Decorate your home</motion.span> 
+            <motion.span {...headerSpan2Animation}> with plants</motion.span>
           </h2>
-          <p className='text'>
+          <motion.p {...mainTextAnimation} className='text'>
             Transform your living space into a vibrant oasis by adorning it with lush green plants, adding a touch of natural beauty and a breath of fresh air to your home decor. 
             With their serene presence and countless benefits, plants bring life and tranquility to any room, creating a harmonious environment that soothes the mind and invigorates the soul.
-          </p>
+          </motion.p>
         </div>    
 
         <ul className='icon_text_list'>
           <li className='list_item'>
-            <div className='icon_wrapper'>
+            <motion.div {...generalIconAnimation} transition={{duration: 0.5, delay: 0.4}} className='icon_wrapper'>
               <img src="images/home-images/quality_icon.webp" alt="quality-icon" />
-            </div>
-            <div className='content_wrapper'>
+            </motion.div>
+            <motion.div {...generalIconContentAnimation} transition={{duration: 0.5, delay: 0.5}} className='content_wrapper'>
               <p>Unbeatable quality</p>
               <p>Our products boast an unbeatable quality that sets them apart from the competition.</p>
-            </div>
+            </motion.div>
           </li>
           <li className='list_item'>
-            <div className='icon_wrapper'>
+            <motion.div {...generalIconAnimation} transition={{duration: 0.5, delay: 0.5}} className='icon_wrapper'>
               <img src="images/home-images/delivery_icon.webp" alt="delivery-icon" />
-            </div>
-            <div className='content_wrapper'>
+            </motion.div>
+            <motion.div {...generalIconContentAnimation} transition={{duration: 0.5, delay: 0.6}} className='content_wrapper'>
               <p>Fast delivery to your door</p>
               <p>Enjoy fast door-to-door delivery for your convenience.</p>
-            </div>
+            </motion.div>
           </li>
           <li className='list_item'>
-            <div className='icon_wrapper'>
+            <motion.div {...generalIconAnimation} transition={{duration: 0.5, delay: 0.6}} className='icon_wrapper'>
               <img src="images/home-images/plant_icon.webp" alt="plant-icon" />
-            </div>
-            <div className='content_wrapper'>
+            </motion.div>
+            <motion.div {...generalIconContentAnimation} transition={{duration: 0.5, delay: 0.7}} className='content_wrapper'>
               <p>Infuse your life with nature</p>
               <p>Embrace nature&apos;s essence and enhance your life.</p>
-            </div>
+            </motion.div>
           </li>
         </ul>
       </article>
 
       <div className='img_container'>
-        <img src="images/home-images/decorate_plant.webp" alt="plant" />
+        <motion.img {...plantImageAnimation} src="images/home-images/decorate_plant.webp" alt="plant" />
       </div>
 
     </section>
--- src/pages/home/components/HeroSection.jsx
+++ src/pages/home/components/HeroSection.jsx
@@ -1,52 +1,90 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const HeroSection = () => {
+  const subHeaderAnimation = {
+    initial: { opacity: 0, top: -30 },
+    whileInView: { opacity: 1, top: 0 },
+    transition: { duration: 0.5 },
+    viewport: { once: true }
+  }
+  const headerAnimation = {
+    initial: { opacity: 0, top: -60 },
+    whileInView: { opacity: 1, top: 0 },
+    transition: { duration: 0.5, delay: 0.2 },
+    viewport: { once: true }
+  }
+  const mainTextAnimation = {
+    initial: { opacity: 0, top: -60 },
+    whileInView: { opacity: 1, top: 0 },
+    transition: { duration: 0.5, delay: 0.3 },
+    viewport: { once: true }
+  }
+  const shopNowBtnAnimation = {
+    initial: { opacity: 0, right: -100, rotate: -10 },
+    whileInView: { opacity: 1, right: 0, rotate: 0 },
+    transition: { duration: 0.5, delay: 0.4 },
+    viewport: { once: true }
+  }
+  const sliderFadeInAnimation = {
+    initial: { opacity: 0, right: -400, rotate: 10, zIndex: 2 },
+    whileInView: { opacity: 1, right: 0, rotate: 0},
+    transition: { duration: 0.5, delay: 0.5 },
+    viewport: { once: true }
+  }
+
   return (
     <section className='hero_section'>
       <article className='hero_article'>
         <div className='text-wrapper'>
-          <p className='sub_header'>TAKE A PLANT</p>
-          <h1>
-            Get 20% Off 
+          <motion.p {...subHeaderAnimation} className='sub_header'>
+            TAKE A PLANT
+          </motion.p>
+          <motion.h1 {...headerAnimation}>
+            Get 20% Off
             Order Now
-          </h1>
-          <p className='main_text'>
-            Looking to enhance your indoor or outdoor space with vibrant foliage and captivating blooms? 
-            Look no further than our premium plant shop! 
-            For a limited time only, we&apos;re thrilled to offer an exclusive discount of 20% off on all orders. 
+          </motion.h1>
+          <motion.p {...mainTextAnimation} className='main_text'>
+            Looking to enhance your indoor or outdoor space with vibrant foliage and captivating blooms?
+            Look no further than our premium plant shop!
+            For a limited time only, we&apos;re thrilled to offer an exclusive discount of 20% off on all orders.
             Don&apos;t miss out on this incredible opportunity to cultivate your very own green oasis at unbeatable prices.
-          </p>
+          </motion.p>
         </div>
 
-        <Link to={"/shop"} className='shop_now_btn'>SHOP NOW<FontAwesomeIcon className='cart_icon' icon={faCartShopping} /></Link>
+        <motion.div style={{ position: "relative" }} {...shopNowBtnAnimation}>
+          <Link to={"/shop"} className='shop_now_btn'>SHOP NOW<FontAwesomeIcon className='cart_icon' icon={faCartShopping} /></Link>
+        </motion.div>
 
       </article>
 
-      <Swiper className="image_slider"
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay]}
-      >
-        <SwiperSlide className='image_item'>
-          <img src="images/home-images/hero-plant4.webp" alt="plant-image" />
-        </SwiperSlide>
-        <SwiperSlide className='image_item'>
-          <img src="images/home-images/hero-plant1.webp" alt="plant-image" />
-        </SwiperSlide>
-        <SwiperSlide className='image_item'>
-          <img src="images/home-images/hero-plant2.webp" alt="plant-image" />
-        </SwiperSlide>
-        <SwiperSlide className='image_item'>
-          <img src="images/home-images/hero-plant3.webp" alt="plant-image" />
-        </SwiperSlide>
-      </Swiper>
+      <motion.div {...sliderFadeInAnimation} className='image_slide_container'>
+        <Swiper className="image_slider"
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+        >
+          <SwiperSlide className='image_item'>
+            <img src="images/home-images/hero-plant4.webp" alt="plant-image" />
+          </SwiperSlide>
+          <SwiperSlide className='image_item'>
+            <img src="images/home-images/hero-plant1.webp" alt="plant-image" />
+          </SwiperSlide>
+          <SwiperSlide className='image_item'>
+            <img src="images/home-images/hero-plant2.webp" alt="plant-image" />
+          </SwiperSlide>
+          <SwiperSlide className='image_item'>
+            <img src="images/home-images/hero-plant3.webp" alt="plant-image" />
+          </SwiperSlide>
+        </Swiper>
+      </motion.div>
     </section>
   )
 }
