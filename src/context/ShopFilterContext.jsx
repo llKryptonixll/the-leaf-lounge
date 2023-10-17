2fce7ba95fb57e113a71b21d0971958524af31db
Lucas Cerri <lucascerri3@outlook.de>
2023-10-17 06:03:10 +0200

    added price filter to the shop

--- src/context/ShopFilterContext.jsx
+++ src/context/ShopFilterContext.jsx
@@ -5,14 +5,25 @@ const ShopFilterContext = createContext();
 
 export function ShopFilterProvider({ children }) {
     const [selectedCategory, setSelectedCategory] = useState("All");
+    const [priceFilter, setPriceFilter] = useState(null);
     const allPlants = useMemo(() => Object.values(data.plantsData).flat(), []);
-
+  
     const filteredPlants = useMemo(() => {
-        return selectedCategory === "All" ? allPlants : data.plantsData[selectedCategory.toLowerCase()];
-    }, [selectedCategory, allPlants]);
+      let plantsToDisplay = selectedCategory === "All" ? allPlants : data.plantsData[selectedCategory.toLowerCase()];
+  
+      if (priceFilter) {
+        plantsToDisplay = plantsToDisplay.filter((plant) => plant.price <= priceFilter);
+      }
+  
+      return plantsToDisplay;
+    }, [selectedCategory, allPlants, priceFilter]);
+  
+    function filterByPrice(price) {
+      setPriceFilter(price);
+    }
 
     return (
-        <ShopFilterContext.Provider value={{ filteredPlants, setSelectedCategory, selectedCategory }}>
+        <ShopFilterContext.Provider value={{ filteredPlants, setSelectedCategory, selectedCategory, filterByPrice }}>
             {children}
         </ShopFilterContext.Provider>
     )
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -5,53 +5,72 @@ import ShopFilterContext from '../../context/ShopFilterContext';
 
 const Shop = () => {
 
-  const { selectedCategory, setSelectedCategory, filteredPlants } = useContext(ShopFilterContext);
+  const { selectedCategory, setSelectedCategory, filteredPlants, filterByPrice } = useContext(ShopFilterContext);
   const [isOpen, setIsOpen] = useState(false);
   const [currentItem, setCurrentItem] = useState({});
+  const [rangeValue, setRangeValue] = useState(10);
+
+  function handleRangeInput(e) {
+    filterByPrice(e.target.value);
+    setRangeValue(e.target.value);
+  }
 
   return (
     <main className='main_shop'>
       <section className="shop_section">
         <p className='header'>Shop</p>
-        <nav className='shop_filter'>
-          <ul className='filter_list'>
-            <li className='filter_item'>
-              <button onClick={() => setSelectedCategory("All")} className={selectedCategory === "All" ? "activeButton" : ""}>All</button>
-            </li>
-            <li className='filter_item'>
-              <button onClick={() => setSelectedCategory("Succulents")} className={selectedCategory === "Succulents" ? "activeButton" : ""}>Succulents</button>
-            </li>
-            <li className='filter_item'>
-              <button onClick={() => setSelectedCategory("Potter_Plants")} className={selectedCategory === "Potter_Plants" ? "activeButton" : ""}>Potter Plants</button>
-            </li>
-            <li className='filter_item'>
-              <button onClick={() => setSelectedCategory("Terrariums")} className={selectedCategory === "Terrariums" ? "activeButton" : ""}>Terrariums</button>
-            </li>
-            <li className='filter_item'>
-              <button onClick={() => setSelectedCategory("Small_Plants")} className={selectedCategory === "Small_Plants" ? "activeButton" : ""}>Small Plants</button>
-            </li>
-            <li className='filter_item'>
-              <button onClick={() => setSelectedCategory("Cactuses")} className={selectedCategory === "Cactuses" ? "activeButton" : ""}>Cactuses</button>
-            </li>
-          </ul>
-        </nav>
+        <div className='filter_options_container'>
+          <nav className='shop_filter'>
+            <p className='filter_header'>Filter by Categorie:</p>
+            <ul className='filter_list'>
+              <li className='filter_item'>
+                <button onClick={() => setSelectedCategory("All")} className={selectedCategory === "All" ? "activeButton" : ""}>All</button>
+              </li>
+              <li className='filter_item'>
+                <button onClick={() => setSelectedCategory("Succulents")} className={selectedCategory === "Succulents" ? "activeButton" : ""}>Succulents</button>
+              </li>
+              <li className='filter_item'>
+                <button onClick={() => setSelectedCategory("Potter_Plants")} className={selectedCategory === "Potter_Plants" ? "activeButton" : ""}>Potter Plants</button>
+              </li>
+              <li className='filter_item'>
+                <button onClick={() => setSelectedCategory("Terrariums")} className={selectedCategory === "Terrariums" ? "activeButton" : ""}>Terrariums</button>
+              </li>
+              <li className='filter_item'>
+                <button onClick={() => setSelectedCategory("Small_Plants")} className={selectedCategory === "Small_Plants" ? "activeButton" : ""}>Small Plants</button>
+              </li>
+              <li className='filter_item'>
+                <button onClick={() => setSelectedCategory("Cactuses")} className={selectedCategory === "Cactuses" ? "activeButton" : ""}>Cactuses</button>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="price_filter_container">
+            <p className='filter_header'>Filter by Price:</p>
+            <div className='input_container'>
+              <input className='range_slider' type="range" name="" id="" onChange={handleRangeInput} step={0.01} min={10} max={30} value={rangeValue} />
+              <span>${rangeValue}</span>
+            </div>
+          </div>
+        </div>
 
         <div className="shop_items_container">
-          {filteredPlants.map((plant) => {
-            return (
-              <ShopItem
-                key={plant.id}
-                item={plant}
-                id={plant.id}
-                name={plant.name}
-                price={plant.price}
-                image={plant.image}
-                inStock={plant.in_stock}
-                setIsOpen={setIsOpen}
-                setCurrentItem={setCurrentItem}
-              />
-            )
-          })}
+          {
+            filteredPlants.length === 0 ? <p className='no_products_error'>No Products Found!</p> : filteredPlants.map((plant) => {
+              return (
+                <ShopItem
+                  key={plant.id}
+                  item={plant}
+                  id={plant.id}
+                  name={plant.name}
+                  price={plant.price}
+                  image={plant.image}
+                  inStock={plant.in_stock}
+                  setIsOpen={setIsOpen}
+                  setCurrentItem={setCurrentItem}
+                />
+              )
+            })
+          }
         </div>
         <ShopItemDescription
           isOpen={isOpen}
